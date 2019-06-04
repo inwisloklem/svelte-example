27bbffc59e195ea1bdd4ef4d033068abb9ab6a06
Filippov Ilya <filippov2@rg.ru>
2019-06-04 18:45:58 +0300

    Init commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  const name = 'Svelte3'
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Learn {name}</h1>
+<a href="//svelte.dev/tutorial/basics">https://svelte.dev/tutorial/basics</a>
